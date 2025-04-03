c1f3a14d1e2b6c993cee0a99932015762719e462
defDimych <def.dimych@gmail.com>
2025-04-03 20:54:25 +0200

    Adding validation

--- src/modules/user-accounts/domain/account-data.schema.ts
+++ src/modules/user-accounts/domain/account-data.schema.ts
@@ -1,11 +1,25 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
+export const loginConstraints = {
+  minLength: 3,
+  maxLength: 10,
+};
+
+export const passwordConstraints = {
+  minLength: 6,
+  maxLength: 20,
+};
+
+export const emailConstraints = {
+  match: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
+};
+
 @Schema({ _id: false, timestamps: true })
 export class AccountData {
-  @Prop({ type: String, required: true })
+  @Prop({ type: String, required: true, ...loginConstraints, unique: true })
   login: string;
 
-  @Prop({ type: String, required: true })
+  @Prop({ type: String, required: true, ...emailConstraints, unique: true })
   email: string;
 
   @Prop({ type: String, required: true })
--- src/modules/user-accounts/dto/create-user.dto.ts
+++ src/modules/user-accounts/dto/create-user.dto.ts
@@ -1,5 +1,23 @@
+import { IsStringWithTrim } from '../../../core/decorators/validation/is-string-with-trim';
+import {
+  loginConstraints,
+  passwordConstraints,
+} from '../domain/account-data.schema';
+import { IsEmail, IsString } from 'class-validator';
+import { Trim } from '../../../core/decorators/transform/trim';
+
 export class CreateUserDto {
-  email: string;
+  @IsStringWithTrim(loginConstraints.minLength, loginConstraints.maxLength)
   login: string;
+
+  @IsStringWithTrim(
+    passwordConstraints.minLength,
+    passwordConstraints.maxLength,
+  )
   password: string;
+
+  @IsString()
+  @IsEmail()
+  @Trim()
+  email: string;
 }
