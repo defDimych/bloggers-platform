b1defd585708b003b1ebcb17fe9cc7ca28caf5a0
defDimych <def.dimych@gmail.com>
2025-04-04 23:10:25 +0200

    Errors transformation class-validator.

--- src/setup/pipes.setup.ts
+++ src/setup/pipes.setup.ts
@@ -1,9 +1,52 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { ValidationError } from 'class-validator';
+import {
+  DomainException,
+  Extension,
+} from '../core/exceptions/domain-exceptions';
+import { DomainExceptionCode } from '../core/exceptions/domain-exception-codes';
+
+export const errorFormatter = (
+  errors: ValidationError[],
+  errorMessage?: Extension[] | [],
+): Extension[] => {
+  const errorsForResponse = errorMessage || [];
+
+  for (const error of errors) {
+    if (!error?.constraints && error?.children?.length) {
+      errorFormatter(error.children, errorsForResponse);
+    } else if (error?.constraints) {
+      const constraintKeys = Object.keys(error.constraints);
+
+      for (const key of constraintKeys) {
+        errorsForResponse.push({
+          message: error.constraints[key]
+            ? `${error.constraints[key]}; Received value: ${error?.value}`
+            : '',
+          key: error.property,
+        });
+      }
+    }
+  }
+
+  return errorsForResponse;
+};
 
 export function pipesSetup(app: INestApplication) {
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
+      stopAtFirstError: true,
+      //Для преобразования ошибок класс валидатора в необходимый вид
+      exceptionFactory: (errors) => {
+        const formattedErrors = errorFormatter(errors);
+
+        throw new DomainException({
+          message: 'Validation failed',
+          code: DomainExceptionCode.ValidationError,
+          extensions: formattedErrors,
+        });
+      },
     }),
   );
 }
