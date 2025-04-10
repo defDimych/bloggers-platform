e28358ab677422dee64cbe9ddfc483ee27c7f87e
defDimych <def.dimych@gmail.com>
2025-04-10 09:16:51 +0200

    validation

--- src/core/dto/base.query-params.input-dto.ts
+++ src/core/dto/base.query-params.input-dto.ts
@@ -1,20 +1,26 @@
 import { Type } from 'class-transformer';
+import { IsEnum, IsNumber } from 'class-validator';
+
+export enum SortDirection {
+  Asc = 'asc',
+  Desc = 'desc',
+}
 
 export class BaseQueryParams {
   @Type(() => Number)
+  @IsNumber()
   pageNumber: number = 1;
+
   @Type(() => Number)
+  @IsNumber()
   pageSize: number = 10;
+
+  @IsEnum(SortDirection)
   sortDirection: SortDirection = SortDirection.Desc;
 
   calculateSkip() {
     return (this.pageNumber - 1) * this.pageSize;
   }
-}
-
-export enum SortDirection {
-  Asc = 'asc',
-  Desc = 'desc',
 }
 
 // export abstract class baseSortablePaginationParams<T> extends PaginationParams {
--- src/modules/user-accounts/api/input-dto/get-users.query-params.input-dto.ts
+++ src/modules/user-accounts/api/input-dto/get-users.query-params.input-dto.ts
@@ -1,4 +1,5 @@
 import { BaseQueryParams } from '../../../../core/dto/base.query-params.input-dto';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 enum UsersSortBy {
   CreatedAt = 'createdAt',
@@ -7,7 +8,14 @@ enum UsersSortBy {
 }
 
 export class GetUsersQueryParams extends BaseQueryParams {
+  @IsEnum(UsersSortBy)
   sortBy = UsersSortBy.CreatedAt;
+
+  @IsString()
+  @IsOptional()
   searchLoginTerm: string | null = null;
+
+  @IsString()
+  @IsOptional()
   searchEmailTerm: string | null = null;
 }
