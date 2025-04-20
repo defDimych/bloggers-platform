1a0d6dfc99a2b2f8f52545f8c0fbed7c4d93b288
defDimych <def.dimych@gmail.com>
2025-04-20 10:29:43 +0200

    basic-auth.guard

--- src/modules/user-accounts/api/users.controller.ts
+++ src/modules/user-accounts/api/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Param,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UsersService } from '../application/users.service';
@@ -15,8 +16,10 @@ import { UsersQueryRepository } from '../infrastructure/query/users.query-reposi
 import { UsersViewDto } from './view-dto/users.view-dto';
 import { GetUsersQueryParams } from './input-dto/get-users.query-params.input-dto';
 import { PaginatedViewDto } from '../../../core/dto/base.paginated.view-dto';
+import { BasicAuthGuard } from '../guards/basic/basic-auth.guard';
 
 @Controller('users')
+@UseGuards(BasicAuthGuard)
 export class UsersController {
   constructor(
     private usersService: UsersService,
---
+++ src/modules/user-accounts/guards/basic/basic-auth.guard.ts
@@ -0,0 +1,50 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+import { Request } from 'express';
+import { DomainException } from '../../../../core/exceptions/domain-exceptions';
+import { DomainExceptionCode } from '../../../../core/exceptions/domain-exception-codes';
+
+@Injectable()
+export class BasicAuthGuard implements CanActivate {
+  private readonly validUsername = 'admin';
+  private readonly validPassword = 'qwerty';
+
+  constructor(private reflector: Reflector) {}
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<Request>();
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
+      throw new DomainException({
+        code: DomainExceptionCode.Unauthorized,
+        message: 'Unauthorized',
+      });
+    }
+
+    const base64Credentials = authHeader.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString(
+      'utf-8',
+    );
+
+    const [username, password] = credentials.split(':');
+
+    if (username === this.validUsername && password === this.validPassword) {
+      return true;
+    } else {
+      throw new DomainException({
+        code: DomainExceptionCode.Unauthorized,
+        message: 'Unauthorized',
+      });
+    }
+  }
+}
