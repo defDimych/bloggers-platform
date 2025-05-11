061198d019657cf59f28bc1052c47608ca6e84da
defDimych <def.dimych@gmail.com>
2025-05-11 23:19:06 +0200

    registering one class multiple times using tokens

--- src/modules/user-accounts/user-accounts.module.ts
+++ src/modules/user-accounts/user-accounts.module.ts
@@ -9,28 +9,49 @@ import { UsersQueryRepository } from './infrastructure/query/users.query-reposit
 import { AuthController } from './api/auth.controller';
 import { LocalStrategy } from './guards/local/local.strategy';
 import { AuthService } from './application/auth.service';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { NotificationModule } from '../notifications/notification.module';
 import { AuthQueryRepository } from './infrastructure/query/auth.query-repository';
 import { JwtStrategy } from './guards/bearer/jwt.strategy';
 import { BasicAuthGuard } from './guards/basic/basic-auth.guard';
 import { AuthConfig } from './config/auth.config';
+import {
+  ACCESS_TOKEN_STRATEGY_INJECT_TOKEN,
+  REFRESH_TOKEN_STRATEGY_INJECT_TOKEN,
+} from './constants/auth-tokens.inject-constants';
+import { LoginUserUseCase } from './application/usecases/login-user.usecase';
+
+const useCases = [LoginUserUseCase];
 
 @Module({
   imports: [
-    JwtModule.registerAsync({
-      imports: [UserAccountsModule],
-      inject: [AuthConfig],
-      useFactory: (authConfig: AuthConfig) => ({
-        secret: authConfig.accessTokenSecret,
-        signOptions: { expiresIn: authConfig.accessTokenExpireIn },
-      }),
-    }),
+    JwtModule,
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     NotificationModule,
   ],
   controllers: [UsersController, AuthController],
   providers: [
+    ...useCases,
+    {
+      provide: ACCESS_TOKEN_STRATEGY_INJECT_TOKEN,
+      useFactory: (authConfig: AuthConfig): JwtService => {
+        return new JwtService({
+          secret: authConfig.accessTokenSecret,
+          signOptions: { expiresIn: authConfig.accessTokenExpireIn },
+        });
+      },
+      inject: [AuthConfig],
+    },
+    {
+      provide: REFRESH_TOKEN_STRATEGY_INJECT_TOKEN,
+      useFactory: (authConfig: AuthConfig): JwtService => {
+        return new JwtService({
+          secret: authConfig.refreshTokenSecret,
+          signOptions: { expiresIn: authConfig.refreshTokenExpireIn },
+        });
+      },
+      inject: [AuthConfig],
+    },
     UsersService,
     CryptoService,
     UsersRepository,
