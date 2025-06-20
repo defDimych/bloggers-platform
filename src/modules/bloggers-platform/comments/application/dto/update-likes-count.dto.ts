739f3c2eae7afeef249334ffefda68fe172974ca
defDimych <def.dimych@gmail.com>
2025-06-20 20:05:26 +0300

    deleted

--- src/modules/bloggers-platform/comments/application/dto/update-likes-count.dto.ts
+++
@@ -1,7 +0,0 @@
-import { LikeStatus } from '../../../common/types/like-status.enum';
-
-export class UpdateLikesCountDto {
-  commentId: string;
-  likeStatus: LikeStatus;
-  currentStatus: LikeStatus;
-}
