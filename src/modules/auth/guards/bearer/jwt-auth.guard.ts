9cc177069a90012b134de9a6e6ae33b9b97de679
defDimych <def.dimych@gmail.com>
2025-06-27 09:18:41 +0300

    global authentication

--- src/modules/auth/guards/bearer/jwt-auth.guard.ts
+++ src/modules/auth/guards/bearer/jwt-auth.guard.ts
@@ -1,11 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { DomainException } from '../../../../core/exceptions/domain-exceptions';
 import { DomainExceptionCode } from '../../../../core/exceptions/domain-exception-codes';
 import { UserContextDto } from '../dto/user-context.dto';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+    return super.canActivate(context);
+  }
+
   handleRequest<TUser = UserContextDto>(err, user: TUser) {
     if (err || !user) {
       throw new DomainException({
