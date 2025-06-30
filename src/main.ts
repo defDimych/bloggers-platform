54f75d1bf4f00771f65fbcc5a4ae6aeeb5c9cecf
defDimych <def.dimych@gmail.com>
2025-06-30 22:55:54 +0300

    working with environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { appSetup } from './setup/app.setup';
+import { CoreConfig } from './core/core.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const coreConfig = app.get<CoreConfig>(CoreConfig);
+
   appSetup(app); //Global app settings.
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = coreConfig.port;
+
+  await app.listen(port, () => {
+    console.log('App starting listen port: ', port);
+  });
 }
 bootstrap();
