f857d00b2a45618aa7677cc436e6806e5ed941f1
defDimych <def.dimych@gmail.com>
2025-07-03 11:05:02 +0300

    adding environment variables, validation

--- src/core/core.config.ts
+++ src/core/core.config.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { IsNumber, Min } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsNumber, Min } from 'class-validator';
 import { configValidationUtility } from '../setup/config-validation.utility';
 
 @Injectable()
@@ -9,8 +9,25 @@ export class CoreConfig {
   @Min(1, { message: 'Set Env variable PORT, example: 3000' })
   port: number;
 
+  @IsNotEmpty({
+    message:
+      'Set Env variable MONGO_URI, example: mongodb://localhost:27017/blogs-and-posts-nest',
+  })
+  mongoURI: string;
+
+  @IsBoolean({
+    message:
+      'Set Env variable INCLUDE_TESTING_MODULE to enable/disable Dangerous for production TestingModule, example: true, available values: [true, false, 0, 1, enabled, disabled]',
+  })
+  includeTestingModule: boolean | null;
+
   constructor(private configService: ConfigService<any, true>) {
     this.port = Number(this.configService.get('PORT'));
+    this.mongoURI = this.configService.get('MONGO_URI');
+
+    this.includeTestingModule = configValidationUtility.convertToBoolean(
+      this.configService.get('INCLUDE_TESTING_MODULE'),
+    );
 
     configValidationUtility.validateConfig(this);
   }
