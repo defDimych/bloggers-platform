f412dbc06d49f2509e37d30a6f80e398fd21bfc9
defDimych <def.dimych@gmail.com>
2025-08-13 13:26:55 +0300

    create input-dto

---
+++ src/modules/bloggers-platform/blogs/api/input-dto/update-post-for-blog.input-dto.ts
@@ -0,0 +1,20 @@
+import { IsStringWithTrim } from '../../../../../core/decorators/validation/is-string-with-trim';
+import {
+  contentConstraints,
+  shortDescriptionConstraints,
+  titleConstraints,
+} from '../../../posts/domain/post.entity';
+
+export class UpdatePostForBlogInputDto {
+  @IsStringWithTrim(contentConstraints.minLength, contentConstraints.maxLength)
+  content: string;
+
+  @IsStringWithTrim(
+    shortDescriptionConstraints.minLength,
+    shortDescriptionConstraints.maxLength,
+  )
+  shortDescription: string;
+
+  @IsStringWithTrim(titleConstraints.minLength, titleConstraints.maxLength)
+  title: string;
+}
