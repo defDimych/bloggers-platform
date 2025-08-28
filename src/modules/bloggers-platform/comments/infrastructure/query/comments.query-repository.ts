17268ffb719e64d96677e54bc5364c8f25acf21b
defDimych <def.dimych@gmail.com>
2025-08-28 23:16:49 +0300

    migration to postgres getAllCommentsForPost

--- src/modules/bloggers-platform/comments/infrastructure/query/comments.query-repository.ts
+++ src/modules/bloggers-platform/comments/infrastructure/query/comments.query-repository.ts
@@ -14,6 +14,7 @@ import { PaginatedViewDto } from '../../../../../core/dto/base.paginated.view-dt
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { CommentWithUserLoginAndLikesInfo } from '../types/comment-db.types';
+import { GetCommentsQueryParams } from '../../../posts/api/input-dto/get-comments-query-params.input-dto';
 
 @Injectable()
 export class CommentsQueryRepository {
@@ -23,6 +24,74 @@ export class CommentsQueryRepository {
     private CommentLikeModel: CommentLikeModelType,
     @InjectDataSource() private dataSource: DataSource,
   ) {}
+
+  async getAllCommentsForPost(dto: {
+    queryParams: GetCommentsQueryParams;
+    userId: number | null;
+    postId: number;
+  }): Promise<PaginatedViewDto<CommentViewDto[]>> {
+    try {
+      const comments = await this.dataSource.query<
+        CommentWithUserLoginAndLikesInfo[]
+      >(
+        `
+SELECT
+    c.*,
+    u.login AS "userLogin",
+    (
+      SELECT COUNT(*)
+      FROM "CommentsLikes" cl
+      WHERE cl."commentId" = c.id
+        AND cl.status = '${LikeStatus.Like}'
+    ) AS "likesCount",
+    (
+      SELECT COUNT(*)
+      FROM "CommentsLikes" cl
+      WHERE cl."commentId" = c.id
+        AND cl.status = '${LikeStatus.Dislike}'
+    ) AS "dislikesCount",
+    COALESCE((
+      SELECT cl.status
+      FROM "CommentsLikes" cl
+      WHERE cl."commentId" = c.id
+        AND cl."userId" = $1
+    ), '${LikeStatus.None}') AS "myStatus"
+  FROM "Comments" c
+    LEFT JOIN "Users" u ON c."userId" = u.id
+  WHERE c."postId" = $2
+    AND c."deletedAt" IS NULL
+  ORDER BY c."${dto.queryParams.sortBy}" ${dto.queryParams.sortDirection}
+  LIMIT ${dto.queryParams.pageSize}
+  OFFSET ${dto.queryParams.calculateSkip()};
+  `,
+        [dto.userId, dto.postId],
+      );
+
+      const totalCount = await this.dataSource.query<{ totalCount: string }[]>(
+        `
+        SELECT COUNT(*) AS "totalCount"
+        FROM "Comments"
+        WHERE "postId" = $1
+         AND "deletedAt" IS NULL;`,
+        [dto.postId],
+      );
+
+      const items = comments.map(CommentViewDto.mapToView);
+
+      return PaginatedViewDto.mapToView({
+        items,
+        totalCount: Number(totalCount[0].totalCount),
+        page: dto.queryParams.pageNumber,
+        size: dto.queryParams.pageSize,
+      });
+    } catch (err) {
+      console.log(
+        `Comment query repository, getAllCommentsForPost : ${JSON.stringify(err, null, 2)}`,
+      );
+
+      throw new Error(`some error`);
+    }
+  }
 
   async getAll(
     dto: GetAllCommentsDto,
