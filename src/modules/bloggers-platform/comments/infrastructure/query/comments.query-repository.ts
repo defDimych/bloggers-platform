fa3f0091cd867a78081a18b4a20a64939f39f630
defDimych <def.dimych@gmail.com>
2025-08-29 11:06:25 +0300

    delete unused method

--- src/modules/bloggers-platform/comments/infrastructure/query/comments.query-repository.ts
+++ src/modules/bloggers-platform/comments/infrastructure/query/comments.query-repository.ts
@@ -1,14 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Comment, CommentModelType } from '../../domain/comment.entity';
 import { DomainException } from '../../../../../core/exceptions/domain-exceptions';
 import { DomainExceptionCode } from '../../../../../core/exceptions/domain-exception-codes';
 import { CommentViewDto } from '../../api/view-dto/comments.view-dto';
-import {
-  CommentLike,
-  CommentLikeModelType,
-} from '../../../likes/domain/comment-like.entity';
-import { GetAllCommentsDto } from './dto/get-all-comments.dto';
 import { LikeStatus } from '../../../common/types/like-status.enum';
 import { PaginatedViewDto } from '../../../../../core/dto/base.paginated.view-dto';
 import { InjectDataSource } from '@nestjs/typeorm';
@@ -18,12 +11,7 @@ import { GetCommentsQueryParams } from '../../../posts/api/input-dto/get-comment
 
 @Injectable()
 export class CommentsQueryRepository {
-  constructor(
-    @InjectModel(Comment.name) private CommentModel: CommentModelType,
-    @InjectModel(CommentLike.name)
-    private CommentLikeModel: CommentLikeModelType,
-    @InjectDataSource() private dataSource: DataSource,
-  ) {}
+  constructor(@InjectDataSource() private dataSource: DataSource) {}
 
   async getAllCommentsForPost(dto: {
     queryParams: GetCommentsQueryParams;
@@ -87,62 +75,6 @@ SELECT
     } catch (err) {
       console.log(
         `Comment query repository, getAllCommentsForPost : ${JSON.stringify(err, null, 2)}`,
-      );
-
-      throw new Error(`some error`);
-    }
-  }
-
-  async getAll(
-    dto: GetAllCommentsDto,
-  ): Promise<PaginatedViewDto<CommentViewDto[]>> {
-    const filter = dto.userId
-      ? {
-          postId: dto.postId,
-          'commentatorInfo.userId': dto.userId,
-          deletedAt: null,
-        }
-      : { postId: dto.postId, deletedAt: null };
-
-    try {
-      const commentsPromise = this.CommentModel.find(filter)
-        .sort({ [dto.query.sortBy]: dto.query.sortDirection })
-        .skip(dto.query.calculateSkip())
-        .limit(dto.query.pageSize)
-        .lean();
-
-      const totalCountPromise = this.CommentModel.countDocuments(filter);
-      const [comments, totalCount] = await Promise.all([
-        commentsPromise,
-        totalCountPromise,
-      ]);
-
-      const ids = comments.map((comment) => comment._id.toString());
-      const likes = await this.CommentLikeModel.find({
-        commentId: { $in: ids },
-        userId: dto.userId,
-      }).lean();
-
-      const dictionaryLikes = likes.reduce((dict, like) => {
-        dict.set(like.commentId, like.myStatus);
-        return dict;
-      }, new Map<string, LikeStatus>());
-
-      const items: CommentViewDto[] = CommentViewDto.mapManyToView({
-        userId: dto.userId,
-        comments,
-        dictionaryLikes,
-      });
-
-      return PaginatedViewDto.mapToView({
-        items,
-        totalCount,
-        page: dto.query.pageNumber,
-        size: dto.query.pageSize,
-      });
-    } catch (err) {
-      console.log(
-        `Comment query repository, getAllComments : ${JSON.stringify(err, null, 2)}`,
       );
 
       throw new Error(`some error`);
