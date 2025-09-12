a5687d1c8c6e741dd653595527b25650fe5bb330
defDimych <def.dimych@gmail.com>
2025-09-12 21:57:57 +0300

    tests for create user

--- test/users/users.e2e-spec.ts
+++ test/users/users.e2e-spec.ts
@@ -7,6 +7,7 @@ import { BASIC_AUTH_CREDENTIALS } from '../../src/constants';
 import { fromUTF8ToBase64 } from '../helpers/encoder';
 import { GLOBAL_PREFIX } from '../../src/setup/global-prefix.setup';
 import { App } from 'supertest/types';
+import { ErrorResponseBody } from '../../src/core/exceptions/filters/error-response-body.type';
 
 describe('SuperAdminUsersController (e2e)', () => {
   let app: INestApplication<App>;
@@ -27,7 +28,46 @@ describe('SuperAdminUsersController (e2e)', () => {
     await deleteAllData(app);
   });
 
-  describe('delete user', () => {
+  describe('create user', () => {
+    it('should create user', async () => {
+      await userTestHelper.createUser({
+        login: 'Webster',
+        password: 'Webster123',
+        email: 'test@mail.ru',
+      });
+    });
+
+    it('should return 400 if email is not unique', async () => {
+      await userTestHelper.createUser({
+        login: 'Webster',
+        password: 'Webster123',
+        email: 'test@mail.ru',
+      });
+
+      const response = await request(app.getHttpServer())
+        .post(`/${GLOBAL_PREFIX}/sa/users`)
+        .set({
+          Authorization:
+            'Basic ' +
+            fromUTF8ToBase64(
+              BASIC_AUTH_CREDENTIALS.username,
+              BASIC_AUTH_CREDENTIALS.password,
+            ),
+        })
+        .send({
+          login: 'Teacher',
+          password: 'Webster123',
+          email: 'test@mail.ru',
+        })
+        .expect(HttpStatus.BAD_REQUEST);
+
+      const body = response.body as ErrorResponseBody;
+
+      expect(body.errorsMessages[0].field).toBe('email');
+    });
+  });
+
+  describe.skip('delete user', () => {
     it('should delete user', async () => {
       const createdUser = await userTestHelper.createUser({
         login: 'Webster',
