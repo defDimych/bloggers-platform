0210214bd7105b7b3ddf3bfb459cb2c89f3ee365
defDimych <def.dimych@gmail.com>
2025-09-13 07:48:39 +0300

    register typeorm entities

--- src/modules/user-accounts/user-accounts.module.ts
+++ src/modules/user-accounts/user-accounts.module.ts
@@ -1,7 +1,5 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { SuperAdminUsersController } from './api/super-admin-users.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './domain/user.entity';
 import { UsersRepository } from './infrastructure/users.repository';
 import { UsersQueryRepository } from './infrastructure/query/users.query-repository';
 import { UsersFactory } from './application/factories/users.factory';
@@ -9,13 +7,17 @@ import { CreateUserUseCase } from './application/usecases/create-user.usecase';
 import { DeleteUserUseCase } from './application/usecases/delete-user.usecase';
 import { UsersService } from './application/services/users.service';
 import { AuthModule } from '../auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { EmailConfirmation } from './entities/email-confirmation.entity';
+import { PasswordRecovery } from './entities/password-recovery.entity';
 
 const useCases = [CreateUserUseCase, DeleteUserUseCase];
 
 @Module({
   imports: [
     forwardRef(() => AuthModule),
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    TypeOrmModule.forFeature([User, EmailConfirmation, PasswordRecovery]),
   ],
   controllers: [SuperAdminUsersController],
   providers: [
