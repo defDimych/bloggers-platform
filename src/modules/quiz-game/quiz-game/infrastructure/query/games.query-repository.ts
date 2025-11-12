b6953d637c02cd0ac45d4895292d81a3762a8645
defDimych <def.dimych@gmail.com>
2025-11-12 21:12:06 +0300

    rewritten query using repository

--- src/modules/quiz-game/quiz-game/infrastructure/query/games.query-repository.ts
+++ src/modules/quiz-game/quiz-game/infrastructure/query/games.query-repository.ts
@@ -2,50 +2,43 @@ import { Injectable } from '@nestjs/common';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { Game } from '../../entities/game.entity';
-import { GameStatus } from '../../common/game-status.enum';
+import { GameViewDto } from '../../api/view-dto/games.view-dto';
 
 @Injectable()
 export class GamesQueryRepository {
   constructor(@InjectDataSource() private dataSource: DataSource) {}
 
-  async findById(dto: { id: string }) {
-    const result = await this.dataSource
-      .getRepository(Game)
-      .createQueryBuilder('g')
-      .select('g.status', 'status')
-      .where('g.id = :id', { id: dto.id })
-      .getRawOne<{ status: GameStatus } | undefined>();
+  async findById(dto: { id: string }): Promise<GameViewDto> {
+    const game = await this.dataSource.getRepository(Game).findOne({
+      select: {
+        id: true,
+        status: true,
+        createdAt: true,
+        startedAt: true,
+        finishedAt: true,
+      },
+      relations: {
+        firstPlayer: {
+          user: true,
+          answers: true,
+        },
+        secondPlayer: {
+          user: true,
+          answers: true,
+        },
+        gameQuestions: {
+          question: true,
+        },
+      },
+      where: {
+        id: dto.id,
+      },
+    });
 
-    const status = result!.status;
-
-    const builder = this.dataSource
-      .getRepository(Game)
-      .createQueryBuilder('g')
-      .select([
-        'g.id as "gameId"',
-        'g.status as "gameStatus"',
-        'g.createdAt as "createdAt"',
-        'g.firstPlayerId as "firstPlayerId"',
-        'fpu.login as "firstPlayerLogin"',
-        'fp.score as "firstPlayerScore"',
-      ])
-      .innerJoin('g.firstPlayer', 'fp', 'fp.id = g."firstPlayerId"')
-      .innerJoin('fp.user', 'fpu', 'fpu.id = fp."userId"')
-      .where('g.id = :id', { id: dto.id });
-
-    if (status !== GameStatus.PendingSecondPlayer) {
-      builder
-        .innerJoin('g.secondPlayer', 'sp', 'sp.id = g."secondPlayerId"')
-        .innerJoin('sp.user', 'spu', 'spu.id = sp."userId"')
-        .addSelect('sp.id', 'secondPlayerId')
-        .addSelect('spu.login', 'secondPlayerLogin')
-        .addSelect('sp.score', 'secondPlayerScore');
+    if (!game) {
+      throw new Error(`Game by id:${dto.id} not found!`);
     }
 
-    const game = await builder.getRawOne();
-
-    console.log(game);
-
-    return game;
+    return GameViewDto.mapToView(game);
   }
 }
